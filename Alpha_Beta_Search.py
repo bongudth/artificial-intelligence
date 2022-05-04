829fb66acf9d5cad45a2ab0bd4dd723f9a5e93a2
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-05-04 09:58:42 +0700

    Alphan Beta Search

---
+++ Alpha_Beta_Search.py
@@ -0,0 +1,78 @@
+from TreeNode import Tree
+
+
+def max_value(node, alpha, beta):
+    if len(node.children) == 0:
+        return node
+    node.value = -10000
+    for child in node.children:
+        temp = min_value(child, alpha, beta)
+        if temp.value > node.value:
+            node.value = temp.value
+        if child.value >= beta:
+            return child
+        if child.value > alpha:
+            alpha = child.value
+    return node
+
+
+def min_value(node, alpha, beta):
+    if len(node.children) == 0:
+        return node
+    node.value = 10000
+    for child in node.children:
+        temp = max_value(child, alpha, beta)
+        if temp.value < node.value:
+            node.value = temp.value
+        if child.value <= alpha:
+            return child
+        if child.value < beta:
+            beta = child.value
+    return node
+
+
+def alpha_beta_search(state):
+    max_value(state, -10000, 10000)
+
+
+if __name__ == "__main__":
+    A = Tree("A")
+    B = Tree("B")
+    C = Tree("C")
+    D = Tree("D")
+    E = Tree("E")
+    F = Tree("F")
+    G = Tree("G")
+    H = Tree("H")
+    I = Tree("I")
+    J = Tree("J")
+    K = Tree("K")
+    L = Tree("L")
+    M = Tree("M")
+    N = Tree("N")
+    Z = Tree("Z")
+    A.add_child(B)
+    A.add_child(C)
+    B.add_child(D)
+    B.add_child(E)
+    C.add_child(F)
+    C.add_child(G)
+    D.add_child(H)
+    D.add_child(I)
+    E.add_child(J)
+    E.add_child(K)
+    F.add_child(M)
+    F.add_child(N)
+    G.add_child(L)
+    G.add_child(Z)
+    H.value = 2
+    I.value = 9
+    J.value = 7
+    K.value = 4
+    M.value = 8
+    N.value = 9
+    L.value = 3
+    Z.value = 5
+
+    alpha_beta_search(A)
+    print(A.value) 
